50c979e2c1de1524cf4d832e1cdbd7d81b736c2a
Frankium <tis7bfrankie@gmail.com>
2020-12-29 07:49:13 +0800

    setup basic responce function

--- AogokeClient.py
+++ AogokeClient.py
@@ -3,8 +3,17 @@ import Utils
 from Utils.Log import log
 
 class AogokeClient(discord.Client):
-    async def on_ready(self):
-        log('Logged on as {0}!'.format(self.user))
+	active_state = False
+	active_user = None
 
-    async def on_message(self, message):
-        log('Message from {0.author}: {0.content}'.format(message))
+	async def callActivate(self,channel,user):
+		await channel.send("我在!")
+		self.active_state = True
+		self.active_user = user.id
+
+	async def on_ready(self):
+		log('Logged on as {0}!'.format(self.user))
+
+	async def on_message(self, message):
+		if (f"<@!{self.user.id}>" in message.content) and (not self.active_state):
+			await self.callActivate(message.channel, message.author)
