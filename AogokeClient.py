70d1792ca60acddf2978b3a6814ecbc46d54d8a4
Frankium <tis7bfrankie@gmail.com>
2020-12-29 14:26:06 +0800

    add basic user request handling methods

--- AogokeClient.py
+++ AogokeClient.py
@@ -14,11 +14,19 @@ class AogokeClient(discord.Client):
 	active_state = False
 	active_user = None
 
-	async def callActivate(self,channel,user):
-		await channel.send("我在!")
+	async def callActivate(self,context):
+		await context.channel.send("我在!")
 		self.active_state = True
-		self.active_user = user.id
-		await client.change_presence(status=discord.Status.dnd)
+		self.active_user = context.author.id
+		await self.change_presence(status=discord.Status.dnd)
+
+	async def callDeactivation(self, context):
+		await context.channel.send("有別的事情的話請再叫我!")
+		self.active_state = False
+
+	async def processRequests(self, context):
+		await context.channel.send("Test")
+		await self.callDeactivation(context)
 
 	async def on_ready(self):
 		log('Logged on as {0}!'.format(self.user))
@@ -26,7 +34,9 @@ class AogokeClient(discord.Client):
 
 	async def on_message(self, message):
 		if (f"<@!{self.user.id}>" in message.content) and (not self.active_state):
-			await self.callActivate(message.channel, message.author)
+			await self.callActivate(message)
+		elif self.active_state and (self.active_user == message.author.id):
+			await self.processRequests(message)
 
 	async def background(self):
 		await asyncio.sleep(0.3)
